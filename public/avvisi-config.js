53e54c0be1c01deb0d2e3b45f3988544be11d09b
Matteo Del Fabbro <6kctmmjpvb@privaterelay.appleid.com>
2026-09-09 19:34:59 +0200

    Collega il pannello al servizio Google

--- public/avvisi-config.js
+++ public/avvisi-config.js
@@ -1,4 +1,4 @@
 window.AVVISI_ADMIN_CONFIG = Object.freeze({
-  endpoint: '',
-  mode: 'draft'
+  endpoint: 'https://script.google.com/a/macros/collegiodimesse.org/s/AKfycbzhWcQfEPqaGl0PaBM0VpbxujvZpP-E1PSjdbzDLmDqH0Lgy3WRsqo5FuHo9eyW5uRy/exec',
+  mode: 'refactor'
 });
