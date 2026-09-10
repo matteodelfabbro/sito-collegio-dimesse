78934358bcedcac2683b4ff30f03d054fe769d34
Matteo Del Fabbro <6kctmmjpvb@privaterelay.appleid.com>
2026-09-10 11:16:23 +0200

    Prepara il pannello per la pubblicazione su main

--- public/avvisi-config.js
+++ public/avvisi-config.js
@@ -1,5 +1,5 @@
 window.AVVISI_ADMIN_CONFIG = Object.freeze({
   endpoint: 'https://script.google.com/a/macros/collegiodimesse.org/s/AKfycbzhWcQfEPqaGl0PaBM0VpbxujvZpP-E1PSjdbzDLmDqH0Lgy3WRsqo5FuHo9eyW5uRy/exec',
-  mode: 'refactor',
+  mode: 'main',
   publicOrigin: 'https://www.collegiodimesse.org'
 });
